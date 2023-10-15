025f90169e837551b3bb4dca66baa94e4ff7d4ba
Alquser Abdullah <codingwork82@gmail.com>
2023-10-15 17:38:24 +0300

    react router refresh prolem fixed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter, HashRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Add from './pages/Add/Add'
 import Watched from './pages/Watched/Watched'
@@ -8,19 +8,19 @@ import Watchlist from './pages/Watchlist/Watchlist'
 import  ContextProvider  from './Context/GlobelContext';
 function App() {
   return (
-    <HashRouter basename='/moviemagic'>
+    <BrowserRouter basename='/moviemagic'>
 
       <ContextProvider>
         <Header />
         <Routes>
           
-          <Route exact path='/' element={<Add />} />
+          <Route exact  path='/' element={<Add />} />
           <Route path='/add' element={<Add/>} />
           <Route path='/watchlist' element={<Watchlist/>} />
           <Route path='/watched' element={<Watched/>} />
         </Routes>
       </ContextProvider>
-    </HashRouter>
+    </BrowserRouter>
 
   );
 }
